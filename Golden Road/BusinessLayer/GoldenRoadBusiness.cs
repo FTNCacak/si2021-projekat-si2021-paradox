3edf4ac846aac5811ac5118a27f942a052fc9a29
Petar Matijević <66-2019@ftn.edu.rs>
2022-01-09 21:32:51 +0100

    Napravljena dodatak BisinssLayer-a na već postojeća dokumenta, ovaj sloj sadržai potrebne delove biznis sloja aplikacije.

---
+++ Golden Road/BusinessLayer/GoldenRoadBusiness.cs
@@ -0,0 +1,30 @@
+﻿using DataLayer;
+using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class GoldenRoadBusiness
+    {
+        readonly GoldenRoadRepository goldenRoadtRepository = new GoldenRoadRepository();
+
+        public List<GoldenRoad> GetUsers()
+        {
+            return goldenRoadtRepository.GetAllUsers();
+        }
+
+        public bool InsertUsers(User user)
+        {
+            return goldenRoadtRepository.InsertUser(user) != 0;
+        }
+
+        public bool InsertPayment(Payment payment)
+        {
+            return goldenRoadtRepository.InsertPayment(payment) != 0;
+        }
+    }
+}
